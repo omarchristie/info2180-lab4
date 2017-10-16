1c4421c30a6e8e53562c82bc50ee20951504d3a3
omarchristie <32620074+omarchristie@users.noreply.github.com>
2017-10-16 07:34:02 -0500

    Excercise 3

--- maze.js
+++ maze.js
@@ -1,7 +1,11 @@
 function setUpEvent() {
     var lose = document.getElementById("boundary1");
     var loseAll = document.querySelectorAll(".boundary");
-    
+    var win = document.getElementById("end");
+    //var lose2 = lose.getClass();
+    console.log(lose.onmouseover = function(){});
+    var test = "";
+    //console.log(lose2);
     lose.onmouseover = function(){
         lose.setAttribute("class", "boundary youlose");
     }
@@ -9,10 +13,19 @@ function setUpEvent() {
     
 	for (var i = 0; i < loseAll.length-1; i++) {
 		loseAll[i].onmouseover = function(){
+			test= "good1";
 			for (var i = 0; i < loseAll.length-1; i++) {
 				loseAll[i].setAttribute("class", "boundary youlose");
 				}
 			}
+	}
+
+	win.onmouseover = function(){
+		if (test == "good1") {
+			console.log("You Lose!!!");
+		} else {
+			console.log("You Win");
+		}
 	}
        
 }
